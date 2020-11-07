9446aa7995df4057feac1b07a4b0b9da30f6f3a0
adrianbparra <adrianplane12@gmail.com>
2020-11-06 19:31:11 -0700

    Completed first excersice
    Create an dict and found the index of the two values that total the limit

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,32 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    # weights is an array
 
-    return None
+    # length is the length of weights
+
+    # limit is the total limit to get
+
+    # if lenth of one return None
+    if length > 1 :
+        
+        
+        weightsDict = {j:i for i,j in enumerate(weights)}
+        # loop over weights
+        # print(weightsDict)
+
+        for i in range(len(weights)):
+            remainder = limit - weights[i]
+
+            if remainder in weightsDict:
+                
+                index = [i,weightsDict[remainder]]
+                index.sort(reverse = True)
+                return index
+            # print(remainder)
+
+            
+        # check if curr index - weight is in the weights dic
+            # return the index
+    else:
+        return None
