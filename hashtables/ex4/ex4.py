33d18faa1b8f1bf47ec1e3c2f17267824f557bad
adrianbparra <adrianplane12@gmail.com>
2020-11-06 20:56:31 -0700

    Completed excersise 4
    Skipped 3 and completed 4 by creating dictionary with negative values and looping over positive values while checking dictionary.

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -4,6 +4,19 @@ def has_negatives(a):
     """
     # Your code here
 
+    # make a dictonary with the keys for only negatives
+    dictSearch = {v:0 for k,v in enumerate(a) if v < 0}
+
+    pos = [v for v in a if v > 0]
+    result = []
+
+    # loop over values 
+    for val in pos:
+        if -val in dictSearch:
+            result.append(val)
+
+
+
     return result
 
 
